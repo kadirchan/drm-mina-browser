2a3bc4ce047b32a0c92fa58c9cc84ea1279be333
kadirchan <kadircanbozkurt1905@gmail.com>
2024-03-21 03:16:39 +0300

    wishlist

---
+++ user-interface/app/components/wishlist.tsx
@@ -0,0 +1,39 @@
+"use client";
+import React, { useEffect, useState } from "react";
+import { Card, CardContent, CardDescription, CardFooter } from "@/components/ui/card";
+import "./wishlist.css";
+import { fetchGameData } from "@/lib/api";
+
+const ENDPOINT = "http://localhost:8080/";
+
+export default function WishlistItems() {
+    const [data, setData] = useState(new Array<Game>());
+
+    useEffect(() => {
+        fetchGameData().then((data) => setData(data));
+    }, []);
+    return (
+        <div className=" flex w-full flex-wrap gap-4 justify-center">
+            {data.map((game, index) => {
+                return (
+                    <Card
+                        key={index}
+                        className=" relative card-hover-effect aspect-square w-[300px] "
+                    >
+                        <CardContent className=" absolute flex justify-center items-center aspect-square w-[300px]">
+                            <img
+                                src={ENDPOINT + game.cover}
+                                alt={game.name}
+                                className="w-full h-full object-cover"
+                            />
+                        </CardContent>
+                        <CardFooter className="flex justify-between mt-4">
+                            <h3 className="text-lg font-medium">{game.name}</h3>
+                            <h3 className="text-lg font-medium">{game.price}</h3>
+                        </CardFooter>
+                    </Card>
+                );
+            })}
+        </div>
+    );
+}
--- user-interface/app/wishlist/page.tsx
+++ user-interface/app/wishlist/page.tsx
@@ -1,5 +1,30 @@
+"use client";
 import React from "react";
+import { useRouter } from "next/navigation";
+import WishlistItems from "../components/wishlist";
 
 export default function Wishlist() {
-    return <div>Wishlist</div>;
+    const [wishlist, setWishlist] = React.useState([]);
+    const router = useRouter();
+
+    return (
+        <div className=" p-8">
+            {wishlist.length === 1 ? (
+                <div className=" flex w-full justify-center ">
+                    <h2 className="mb-2 text-lg font-medium tracking-tight">
+                        Your Wishlist Is Empty
+                    </h2>
+
+                    <h3
+                        className="mb-2 text-lg absolute align-middle top-1/2 font-medium tracking-tight underline underline-offset-2 hover:underline-offset-4 cursor-pointer"
+                        onClick={() => router.push("/store")}
+                    >
+                        Explore the store
+                    </h3>
+                </div>
+            ) : (
+                <WishlistItems />
+            )}
+        </div>
+    );
 }
