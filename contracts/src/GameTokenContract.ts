27b7780093b24badba714266c0a9e3650b36a751
kadirchan <kadircanbozkurt1905@gmail.com>
2024-03-29 02:01:30 +0300

    game contract missed some spots

--- contracts/src/GameTokenContract.ts
+++ contracts/src/GameTokenContract.ts
@@ -2,16 +2,10 @@ import {
   Account,
   AccountUpdate,
   AccountUpdateForest,
-  Bool,
   DeployArgs,
-  Field,
   Permissions,
-  Provable,
   PublicKey,
-  Signature,
-  SmartContract,
   State,
-  Struct,
   TokenContract,
   UInt64,
   method,
@@ -39,6 +33,13 @@ export class GameToken extends TokenContract {
    */
   @state(UInt64) timeoutInterval = State<UInt64>();
 
+  /**
+   * Maximum height of the device Merkle tree.
+   * This is used to restrict the number of devices that can be added to the tree.
+   * Number of devices = 2^(maxTreeHeight - 1)
+   */
+  @state(UInt64) maxTreeHeight = State<UInt64>();
+
   deploy(args?: DeployArgs) {
     super.deploy(args);
 
@@ -58,16 +59,16 @@ export class GameToken extends TokenContract {
 
     super.init();
 
-    this.gamePrice.set(UInt64.from(33333));
+    this.gamePrice.set(UInt64.from(333334946));
     this.discount.set(UInt64.from(2121));
     this.timeoutInterval.set(UInt64.from(1000));
+    this.maxTreeHeight.set(UInt64.from(2));
   }
 
   @method mintGameToken(to: PublicKey) {
     const price = this.gamePrice.getAndRequireEquals();
     const discount = this.discount.getAndRequireEquals();
     const publisherAddress = this.publisher.getAndRequireEquals();
-    Provable.log(publisherAddress);
     const totalPrice = price.sub(discount);
 
     this.account.balance.requireBetween(UInt64.zero, UInt64.MAXINT());
@@ -76,7 +77,7 @@ export class GameToken extends TokenContract {
     userAccount.balance.requireEquals(UInt64.zero);
 
     const accountUpdate = AccountUpdate.createSigned(to);
-    accountUpdate.send({ to: publisherAddress, amount: 13212312 });
+    accountUpdate.send({ to: publisherAddress, amount: totalPrice });
 
     this.internal.mint({
       address: to,
@@ -84,25 +85,33 @@ export class GameToken extends TokenContract {
     });
   }
 
+  @method getBalance(address: PublicKey): UInt64 {
+    const account = Account(address, this.deriveTokenId());
+    return account.balance.getAndRequireEquals();
+  }
+
   @method setGamePrice(price: UInt64) {
     this.onlyPublisher();
-    const currentPrice = this.gamePrice.get();
-    this.gamePrice.requireEquals(currentPrice);
+    this.gamePrice.getAndRequireEquals();
     this.gamePrice.set(price);
   }
 
   @method setDiscount(discount: UInt64) {
     this.onlyPublisher();
-    const currentDiscount = this.discount.get();
-    this.discount.requireEquals(currentDiscount);
+    this.discount.getAndRequireEquals();
     this.discount.set(discount);
   }
 
   @method setTimeoutInterval(interval: UInt64) {
     this.onlyPublisher();
-    const currentInterval = this.timeoutInterval.get();
-    this.timeoutInterval.requireEquals(currentInterval);
+    this.timeoutInterval.getAndRequireEquals();
     this.timeoutInterval.set(interval);
+  }
+
+  @method setMaxTreeHeight(height: UInt64) {
+    this.onlyPublisher();
+    this.maxTreeHeight.getAndRequireEquals();
+    this.maxTreeHeight.set(height);
   }
 
   @method setPublisher(publisher: PublicKey) {
