998320489e26b06724fb2dce629a0101ca2aaea6
kadirchan <kadircanbozkurt1905@gmail.com>
2024-04-13 18:53:06 +0300

    DRM contract with session

--- contracts/src/DRMproof.ts
+++ contracts/src/DRMproof.ts
@@ -1,12 +1,8 @@
 import {
-  Account,
   AccountUpdate,
   Field,
   MerkleMapWitness,
   Poseidon,
-  PrivateKey,
-  Proof,
-  Provable,
   PublicKey,
   Signature,
   SmartContract,
@@ -20,94 +16,6 @@ import {
 } from 'o1js';
 import { GameToken } from './GameTokenContract.js';
 import { Identifiers } from './Identifiers.js';
-
-export class DRM extends SmartContract {
-  @state(Field) deviceRoot = State<Field>();
-  @state(Field) sessionRoot = State<Field>();
-  @state(PublicKey) gameTokenAddress = State<PublicKey>();
-
-  @method addDevice(
-    userAddress: PublicKey,
-    previousValue: Field,
-    identifiers: Identifiers,
-    witness: MerkleMapWitness,
-    signature: Signature
-  ) {
-    const gameTokenId = TokenId.derive(
-      this.gameTokenAddress.getAndRequireEquals()
-    );
-
-    const accountUpdate = AccountUpdate.create(userAddress, gameTokenId);
-    const tokenContract = new GameToken(
-      this.gameTokenAddress.getAndRequireEquals()
-    );
-    const tokenBalance = accountUpdate.account.balance.getAndRequireEquals();
-    tokenContract.approveAccountUpdate(accountUpdate);
-
-    tokenBalance.assertGreaterThan(UInt64.zero);
-
-    // Todo add more checks for identifiers
-
-    const [previousRoot, previousKey] =
-      witness.computeRootAndKey(previousValue);
-
-    this.deviceRoot.getAndRequireEquals().assertEquals(previousRoot);
-    previousKey.assertEquals(Poseidon.hash(userAddress.toFields()));
-
-    const identifiersHash = identifiers.hash();
-    const [newRoot, newKey] = witness.computeRootAndKey(identifiersHash);
-    newKey.assertEquals(previousKey);
-
-    signature.verify(userAddress, [identifiersHash]).assertTrue();
-
-    this.deviceRoot.set(newRoot);
-  }
-
-  @method createSession(
-    witness: MerkleMapWitness,
-    oldSessionValue: Field,
-    newSessionValue: Field,
-    proof: DeviceSessionProof
-  ) {
-    proof.verify();
-    this.sessionRoot
-      .getAndRequireEquals()
-      .assertEquals(proof.publicInput.sessionMerkleRoot);
-
-    this.deviceRoot
-      .getAndRequireEquals()
-      .assertEquals(proof.publicInput.deviceMerkleRoot);
-
-    oldSessionValue.assertNotEquals(newSessionValue);
-
-    const [oldRoot, oldKey] = witness.computeRootAndKey(oldSessionValue);
-
-    oldKey.assertEquals(proof.publicOutput);
-
-    this.sessionRoot.getAndRequireEquals().assertEquals(oldRoot);
-
-    const [newRoot, newKey] = witness.computeRootAndKey(newSessionValue);
-
-    newKey.assertEquals(proof.publicOutput);
-
-    this.sessionRoot.set(newRoot);
-  }
-
-  @method setGameTokenAddress(address: PublicKey) {
-    this.gameTokenAddress.getAndRequireEquals();
-    this.gameTokenAddress.set(address);
-  }
-
-  @method getRoot() {
-    return this.deviceRoot.getAndRequireEquals();
-  }
-
-  // TODO: Remove after testing
-  @method setDeviceRoot(root: Field) {
-    this.deviceRoot.getAndRequireEquals();
-    this.deviceRoot.set(root);
-  }
-}
 
 export class SessionPublicInput extends Struct({
   deviceMerkleRoot: Field,
@@ -140,4 +48,116 @@ const ValidateDevice = ZkProgram({
 
 export class DeviceSessionProof extends ZkProgram.Proof(ValidateDevice) {}
 
-// TODO add session constraints to addDevice method
+export class DRM extends SmartContract {
+  @state(Field) deviceRoot = State<Field>();
+  @state(Field) sessionRoot = State<Field>();
+  @state(PublicKey) gameTokenAddress = State<PublicKey>();
+
+  events = {
+    'new-session': Field,
+  };
+
+  @method addDevice(
+    userAddress: PublicKey,
+    devicePreviousValue: Field,
+    identifiers: Identifiers,
+    deviceWitness: MerkleMapWitness,
+    signature: Signature
+    // ,sessionWitness: MerkleMapWitness
+  ) {
+    const gameTokenId = TokenId.derive(
+      this.gameTokenAddress.getAndRequireEquals()
+    );
+
+    const accountUpdate = AccountUpdate.create(userAddress, gameTokenId);
+    const gameTokenContract = new GameToken(
+      this.gameTokenAddress.getAndRequireEquals()
+    );
+    const tokenBalance = accountUpdate.account.balance.getAndRequireEquals();
+    gameTokenContract.approveAccountUpdate(accountUpdate);
+
+    tokenBalance.assertGreaterThan(UInt64.zero);
+
+    // Todo add more checks for identifiers
+
+    const [previousDeviceRoot, previousDeviceKey] =
+      deviceWitness.computeRootAndKey(devicePreviousValue);
+
+    this.deviceRoot.getAndRequireEquals().assertEquals(previousDeviceRoot);
+    previousDeviceKey.assertEquals(Poseidon.hash(userAddress.toFields()));
+
+    const identifiersHash = identifiers.hash();
+    const [computedRoot, computedKey] =
+      deviceWitness.computeRootAndKey(identifiersHash);
+    computedKey.assertEquals(previousDeviceKey);
+
+    signature.verify(userAddress, [identifiersHash]).assertTrue();
+
+    this.deviceRoot.set(computedRoot);
+
+    /**
+     * Enable session leaf for the device
+     * if leaf = 0 device is not enabled
+     * otherwise device is enabled
+     */
+
+    // const [computedSessionRoot, computedSessionKey] = sessionWitness.computeRootAndKey(Field.from(0))
+    // computedSessionKey.assertEquals(identifiersHash)
+    // computedSessionRoot.assertEquals(this.sessionRoot.getAndRequireEquals())
+
+    // const [newSessionRoot, newSessionKey] = sessionWitness.computeRootAndKey(Field.from(1))
+    // this.sessionRoot.set(newSessionRoot)
+  }
+
+  @method createSession(
+    witness: MerkleMapWitness,
+    previousSessionValue: Field,
+    newSessionValue: Field,
+    proof: DeviceSessionProof
+  ) {
+    proof.verify();
+    this.sessionRoot
+      .getAndRequireEquals()
+      .assertEquals(proof.publicInput.sessionMerkleRoot);
+
+    this.deviceRoot
+      .getAndRequireEquals()
+      .assertEquals(proof.publicInput.deviceMerkleRoot);
+
+    previousSessionValue.assertNotEquals(Field.from(0));
+    previousSessionValue.assertNotEquals(newSessionValue);
+
+    const [previousRoot, previousKey] =
+      witness.computeRootAndKey(previousSessionValue);
+
+    previousKey.assertEquals(proof.publicOutput);
+
+    this.sessionRoot.getAndRequireEquals().assertEquals(previousRoot);
+
+    const [newRoot, newKey] = witness.computeRootAndKey(newSessionValue);
+
+    newKey.assertEquals(proof.publicOutput);
+
+    this.sessionRoot.set(newRoot);
+
+    this.emitEvent(
+      'new-session',
+      Poseidon.hash([proof.publicOutput, newSessionValue])
+    );
+  }
+
+  @method setGameTokenAddress(address: PublicKey) {
+    this.gameTokenAddress.getAndRequireEquals();
+    this.gameTokenAddress.set(address);
+  }
+
+  @method getRoot() {
+    return this.deviceRoot.getAndRequireEquals();
+  }
+
+  // TODO: Remove after testing
+  @method setDeviceRoot(root: Field) {
+    this.deviceRoot.getAndRequireEquals();
+    this.deviceRoot.set(root);
+  }
+}
